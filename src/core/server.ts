383686144b2ca72a02f8e97d99b3cb1e2aa0911d
jl115 <joelbern006@gmail.com>
2025-12-14 17:55:56 +0100

    feat: added minio for file storing

--- src/core/server.ts
+++ src/core/server.ts
@@ -2,35 +2,30 @@ import { Elysia, t } from "elysia";
 import { ErrorHandlerMiddleware } from "@/app/middlewares/errorHandler";
 import { AppRouter } from "@/core/routes";
 import { SocketHandler } from "@/app/features/chat/chat.socket";
+import S3Connection from "./s3/s3connection";
 
-
-
-// --- 4. Create and Configure App ---
+// --- Create App ---
 const app = new Elysia();
 
-// 5. MUST register websocket plugin FIRST
-
-// 6. Register all middleware and routes BEFORE listen()
+// Register middleware
 const errorHandler = new ErrorHandlerMiddleware(app);
 const router = new AppRouter(app);
 const socketHandler = new SocketHandler();
+const s3 = S3Connection.instance.getBucket;
 
 errorHandler.register();
-router.registerAll(); // This registers /auth, /users, /profile, /chat (REST)
+router.registerAll();
 
 app.get("/", () => "Server running ✅");
 
-// --- 7. Register the WebSocket handler BEFORE listen() ---
-// It runs on the SAME port as your HTTP server.
+// --- WebSocket & Listen ---
 app
   .ws("/ws/chat", {
     open: (ws) => socketHandler.open(ws),
     message: (ws, message) => socketHandler.message(ws, message),
     close: (ws) => socketHandler.close(ws),
   })
-  .listen(Number(process.env.SOCKET_PORT) || 3000);
-
-app.listen(Number(process.env.PORT) || 3000);
+  .listen(Number(process.env.PORT) || 3000);
 
 console.log(
   `🚀 Server running on http://localhost:${process.env.PORT || 3000}`,
